0acd7de68cfc1e807ffc18dda6eafbbc4d71e15c
James Gary <gmarchesi888@gmail.com>
2019-01-02 15:41:40 -0500

    attempting to add DB connectivity...going to make lunch first.

--- server.js
+++ server.js
@@ -2,18 +2,25 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
-require('dotenv').config({ path: 'variables.env' });
+require("dotenv").config({ path: "variables.env" });
 
 MongoClient.connect(
   process.env.DATABASE,
-  { useNewUrlParser: true }
+  { useNewUrlParser: true },
+  function(err, client) {
+    if (err) return console.log(err);
+    db = client.db("node_crud_190102");
+    app.listen(3000, () => {
+      console.log("Listening on 3000");
+    });
+  }
 );
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, function() {
-  console.log("Listening on Port 3000.");
-});
+// app.listen(3000, function() {
+//   console.log("Listening on Port 3000.");
+// });
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
