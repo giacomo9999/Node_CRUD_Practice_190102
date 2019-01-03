a0ee8305f54abcadc9297169625bbfcd86c22031
James Gary <gmarchesi888@gmail.com>
2019-01-03 17:25:43 -0500

    trying to style EJS files...failing.

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express();
 const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config({ path: "variables.env" });
 
-
 let db;
 
 MongoClient.connect(
@@ -21,16 +20,28 @@ MongoClient.connect(
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set("view engine", "ejs");
+
 // app.listen(3000, function() {
 //   console.log("Listening on Port 3000.");
 // });
 
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  db.collection("quotes")
+    .find()
+    .toArray((err, result) => {
+      if (err) return console.log(err);
+      // renders index.ejs
+      res.render("index.ejs", { quotes: result });
+    });
 });
 
 app.post("/quotes", (req, res) => {
-  db.collection("quotes").save(req.body, (err, result) => {
+  db.collection("quotes").insertOne(req.body, (err, result) => {
     if (err) return console.log(err);
     console.log("Saved to Database.");
     res.redirect("/");
